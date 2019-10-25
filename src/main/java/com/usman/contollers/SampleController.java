47fdc05b5d12bb24cb5d10dd398f96b2f452ae26
Usman Anwar <usmananwar4all@gmail.com>
2019-10-25 10:46:52 +0900

    initial commit

---
+++ src/main/java/com/usman/contollers/SampleController.java
@@ -0,0 +1,21 @@
+package com.usman.contollers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample")
+public class SampleController {
+
+	@Autowired
+	private HttpServletRequest request;
+
+	@RequestMapping("/login")
+	public void login() {
+		System.out.println("Login action is called!!!");
+	}
+
+}
---
+++ src/test/java/com/usman/demo/SampleAuthentication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usman.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAuthentication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
