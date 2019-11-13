df55c868788e721f42a41dfcf3a064ebb8afec95
Usman Anwar <usmananwar4all@gmail.com>
2019-11-13 16:02:33 +0900

    aadf

--- src/main/java/com/usman/contollers/SampleController.java
+++ src/main/java/com/usman/contollers/SampleController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 @RestController
 @RequestMapping("/MS1")
@@ -27,6 +29,9 @@ public class SampleController {
 
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+		// mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
 		System.out.println(mapper.writeValueAsString(request));
 
 	}
