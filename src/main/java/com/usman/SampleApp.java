b812305598635facf71fe0b9d98037d781fd917b
Usman Anwar <usmananwar4all@gmail.com>
2019-11-13 15:43:02 +0900

    aadf

--- src/main/java/com/usman/SampleApp.java
+++ src/main/java/com/usman/SampleApp.java
@@ -1,18 +1,13 @@
 package com.usman;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.usman.aws.CognitoService;
 
 @SpringBootApplication
 public class SampleApp {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(SampleApp.class, args);
-
-		CognitoService service = new CognitoService();
-
-		service.authenticate("usman.a", "usman123");
+		SpringApplication.run(SampleApp.class, args);
 
 	}
 
--- src/main/java/com/usman/contollers/SampleController.java
+++ src/main/java/com/usman/contollers/SampleController.java
@@ -1,8 +1,5 @@
 package com.usman.contollers;
 
-
-
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,24 +7,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.usman.aws.CognitoService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
-@RequestMapping("/sample")
+@RequestMapping("/MS1")
 public class SampleController {
 
-	//Logger logger = LogManager.getLogger(SampleController.class);
+	// Logger logger = LogManager.getLogger(SampleController.class);
 
 	@Autowired
 	private HttpServletRequest request;
 
-	@Autowired
-	private CognitoService cognitoService;
-
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public void login() {
+	@RequestMapping(value = "/action1", method = RequestMethod.GET)
+	public void login() throws JsonProcessingException {
 		System.out.println("Login action is called!!!");
-		cognitoService.authenticate("usman.a", "usman123");
+
+		ObjectMapper mapper = new ObjectMapper();
+		System.out.println(mapper.writeValueAsString(request));
+
 	}
 
 }
